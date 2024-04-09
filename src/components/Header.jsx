dde9146dbcba1aae76542005c4ee16e3f64086e9
Ilya Krass <krasnenkov.ilya@inbox.ru>
2024-04-09 14:40:08 +0400

    Создание проекта React + Vite; Свёрстана альфа первой страницы

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <div className="header-content">
+          <div className="header-conent-item">
+            <img src="#" alt="logo" />
+            <p>my logo</p>
+          </div>
+          <a href="#" className="header-conent-item">
+            <img src="#" alt="user" />
+            <p>
+              Sign in
+            </p>
+          </a>
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+export default function Main() {
+  return (
+    <main>
+      <div className="container">
+        <div className="main-content">
+          <h1>Platform courses</h1>
+          <div className="buttons-main">
+            <button className="btn">
+              Create a course
+            </button>
+            <button className="btn">
+              Open course
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
